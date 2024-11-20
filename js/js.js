d5eb47d84aa77560d8f578ea38eb9dbb1c764f70
Akmal <awccon@gmail.com>
2024-11-19 23:04:21 -0500

    implemented email form

--- js/js.js
+++ js/js.js
@@ -15,7 +15,7 @@ $(window).ready(function(){
         messageBox.style.top = "50%";
         messageBox.style.left = "50%";
         messageBox.style.transform = "translate(-50%, -50%)";
-        messageBox.innerHTML = `<span>Please go to this linkedIn <a href="https://www.linkedin.com/in/akmalkamolov">link</a> to get a copy of my resume</span>`;
+        messageBox.innerHTML = `<span>Please go to this linkedIn <a href="https://www.linkedin.com/in/akmalkamolov">link</a> to get a copy of my resume or </br> you can leave an email.</span>`;
         messageBox.style.backgroundColor = "white";
         messageBox.style.padding = "15px";
         closeBtn.style.position = "absolute";
